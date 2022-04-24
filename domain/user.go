7f0abee2e2f818e7b2ca448458a88070bda5f0eb
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-04-25 00:39:41 +0900

    [fix] user_repository_test.go

--- domain/user.go
+++ domain/user.go
@@ -11,8 +11,8 @@ type Users []User
 type User struct {
     // gorm.Model
     ID        uint        `gorm:"primaryKey,autoincrement" json:"id,omitempty"`
-	CreatedAt *time.Time  `json:"created_at,omitempty"`
-	UpdatedAt *time.Time  `json:"updated_at,omitempty"`
+	CreatedAt *time.Time  `json:"created_at,omitempty" sql:"DEFAULT:current_timestamp"`
+	UpdatedAt *time.Time  `json:"updated_at,omitempty" sql:"DEFAULT:current_timestamp on update current_timestamp"`
 	DeletedAt *time.Time  `json:"deleted_at,omitempty"`
     Name  string `gorm:"size:255;not null" json:"name,omitempty" validate:"required"`
     Email string `gorm:"size:255;not null;unique" json:"email,omitempty" validate:"required,email"`
--- interfaces/database/user_repository_test.go
+++ interfaces/database/user_repository_test.go
@@ -4,22 +4,18 @@ import (
 	"fmt"
 	"regexp"
 	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
-
 	"github.com/ymktmk/golang-clean-architecture/domain"
 	"github.com/ymktmk/golang-clean-architecture/infrastructure"
 	"github.com/ymktmk/golang-clean-architecture/interfaces/database"
 
 	"gorm.io/driver/mysql"
-	// "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDbMock() (*gorm.DB, sqlmock.Sqlmock, error) {
 	sqlDB, mock, err := sqlmock.New()
-	// MySQL
 	mockDB, err := gorm.Open(
 		mysql.Dialector{
 			Config: &mysql.Config{
@@ -31,7 +27,7 @@ func NewDbMock() (*gorm.DB, sqlmock.Sqlmock, error) {
 	return mockDB, mock, err
 }
 
-func DummyHandler(conn *gorm.DB) *infrastructure.SqlHandler {
+func DummyHandler(conn *gorm.DB) database.SqlHandler {
 	sqlHandler := new(infrastructure.SqlHandler)
 	sqlHandler.Conn = conn
 	return sqlHandler
@@ -43,23 +39,30 @@ func TestStore(t *testing.T) {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
 	
+	u := &domain.User{
+		ID: 1,
+		Name: "tomoki",
+		Email: "victas.tt@gmail.com",
+	}
+	
 	// mock設定
 	mock.ExpectBegin()
 	mock.ExpectQuery(regexp.QuoteMeta(
-		`INSERT INTO users (created_at, updated_at, deleted_at, name, email) VALUES(?, ?, ?, ?, ?)`)).
-		WithArgs(time.Now(), time.Now(), nil, "tomoki", "example@gmail.com").
-		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+		`INSERT INTO users (name, email) VALUES (?, ?)`)).
+		WithArgs(u.Name, u.Email).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email"}).AddRow(1, u.Name, u.Email))
 	mock.ExpectCommit()
 
 	// SQL実行
-	u := &domain.User{
-		Name: "tomoki",
-		Email: "example@gmail.com",
+	if err = mockDB.Create(u).Error; err != nil {
+		t.Error(err)
 	}
-	mockDB.Create(u)
-	fmt.Println(u)
+	// fmt.Println(u)
+	
+	// if err := mock.ExpectationsWereMet(); err != nil {
+	// 	t.Errorf("Test Create User: %v", err)
+	// }
 
-	// Repository Test
 	// repo := &database.UserRepository{SqlHandler: DummyHandler(mockDB)}
 	// user, err := repo.Store(u)
 	// fmt.Println(user)
@@ -69,15 +72,6 @@ func TestStore(t *testing.T) {
 }
 
 
-
-
-
-
-
-
-
-
-
 func TestFindById(t *testing.T) {
 	mockDB, mock, err := NewDbMock()
 	if err != nil {
@@ -85,16 +79,25 @@ func TestFindById(t *testing.T) {
 		return
 	}
 
-	var id int = 1
-	// name  := "tomoki"
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM users WHERE id = ?`)).
-	WithArgs(id).
-	WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(id))
-
-	repo := &database.UserRepository{SqlHandler: DummyHandler(mockDB)}
-	user, err := repo.FindById(id)
-	if err != nil {
-		t.Fatalf("failed to find user: %s", err)
+	u := &domain.User{
+		ID: 1,
+		Name: "tomoki",
+		Email: "victas.tt@gmail.com",
 	}
-	fmt.Println(user)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM users WHERE id = ?`)).
+	WithArgs(u.ID).
+	WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email"}).AddRow(u.ID, u.Name, u.Email))
+
+	// repo := &database.UserRepository{SqlHandler: DummyHandler(mockDB)}
+	// user, err := repo.FindById(int(u.ID))
+	// if err != nil {
+	// 	t.Fatalf("failed to find user: %s", err)
+	// }
+	// fmt.Println(user)
+
+	if err = mockDB.Where("id=?", 1).First(&u).Error; err != nil {
+		t.Error(err)
+	}
+	fmt.Println(u)
 } 
